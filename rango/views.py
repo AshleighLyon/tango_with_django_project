a92f7ecb1b0560a9d79ef5c74409d546f1c60260
Ashleigh Lyon <ashleighlyon@msn.com>
2019-01-16 21:10:36 +0000

    Tango with Django Book Exercises Commit Chapter 3.6

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 
+# Creating the view for the index page and adding a link to the about page
 def index(request):
-	return HttpResponse("Rango says hey there partner!")
+	return HttpResponse("Rango says hey there partner! <br/> <a href= ' /rango/about/'> Click for About Page! </a>")
+
+# Creating the view for the about page and adding a link back to index page
+def about(request):
+	return HttpResponse("Rango says here is the about page. <br/> <a href=' /rango/'> Index Page </a>")
 
 
