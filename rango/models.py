05a39c3d6e2e448b02eab66379136a4df91745e7
Ashleigh Lyon <ashleighlyon@msn.com>
2019-01-23 21:58:20 +0000

    Tango with Django Book Exercises Commit Chapter 5.1 - 5.4

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+# When you define a model, you need to specify the list of fields 
+# and their types. 
+
+class Category(models.Model):
+	# Field name is unique (set to true) meaning this field
+	# can be used as a primar key.
+	name = models.CharField(max_length=128, unique=True)
+		
+	def __str__(self): 
+		return self.name
+
+class Page(models.Model):
+	category = models.ForeignKey(Category)
+	title = models.CharField(max_length=128)
+	url = models.URLField()
+	views = models.IntegerField(default=0)
+	
+	# __str__ generates a string representation of the class
+	# similar to a toString() method in java
+	def __str__(self): 
+		return self.title
