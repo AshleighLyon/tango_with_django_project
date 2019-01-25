4562509f4b53bee23f0e237a86fb12e9f7b5e3f1
Ashleigh Lyon <ashleighlyon@msn.com>
2019-01-25 14:26:37 +0000

    Tango with Django Book Exercises Commit Chapter 5.8 - Part 2 & 7 Official Django Tutorial

--- rango/models.py
+++ rango/models.py
@@ -1,27 +1,31 @@
 from django.db import models
 
 # Create your models here.
-# When you define a model, you need to specify the list of fields 
-# and their types. 
+# When you define a model, you need to specify the list of fields
+# and their types.
+
 
 class Category(models.Model):
-	# Field name is unique (set to true) meaning this field
-	# can be used as a primar key.
-	name = models.CharField(max_length=128, unique=True)
-	
-	class Meta:
-		verbose_name_plural = 'Categories'
-		
-	def __str__(self): 
-		return self.name
+    # Field name is unique (set to true) meaning this field
+    # can be used as a primar key.
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
 
 class Page(models.Model):
-	category = models.ForeignKey(Category)
-	title = models.CharField(max_length=128)
-	url = models.URLField()
-	views = models.IntegerField(default=0)
-	
-	# __str__ generates a string representation of the class
-	# similar to a toString() method in java
-	def __str__(self): 
-		return self.title
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    # __str__ generates a string representation of the class
+    # similar to a toString() method in java
+    def __str__(self):
+        return self.title
