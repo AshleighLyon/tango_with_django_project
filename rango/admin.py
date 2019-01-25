6d95b883e99cc344861b9d5716c81c54c8336d0b
Ashleigh Lyon <ashleighlyon@msn.com>
2019-01-25 16:11:42 +0000

    Tango with Django Book Exercises Commit Chapter 5.8 - pg61 exercises complete

--- rango/admin.py
+++ rango/admin.py
@@ -5,5 +5,10 @@ from rango.models import Category, Page
 # Add another class just do same as below calling
 # the admin.site.register() method.
 
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
