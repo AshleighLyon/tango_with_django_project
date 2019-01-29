70c1b905e2eebe421ec0e9a533463b83b8f69113
Ashleigh Lyon <ashleighlyon@msn.com>
2019-01-29 20:42:53 +0000

    Tango with Django Book Exercises Commit Chapter 6.3

--- rango/models.py
+++ rango/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 # When you define a model, you need to specify the list of fields
@@ -11,9 +12,14 @@ class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = 'categories'
 
     def __str__(self):
         return self.name
